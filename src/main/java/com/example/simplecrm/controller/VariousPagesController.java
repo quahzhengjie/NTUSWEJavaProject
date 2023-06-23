6956756ba96df2d9157a760d8b1b64d4abc09c27
quahzhengjie <45572265+quahzhengjie@users.noreply.github.com>
2023-06-23 15:44:18 +0800

    Update VariousPagesController.java
    clean comments - more descriptive

--- src/main/java/com/example/simplecrm/controller/VariousPagesController.java
+++ src/main/java/com/example/simplecrm/controller/VariousPagesController.java
@@ -8,41 +8,42 @@ public class VariousPagesController {
 
   @GetMapping("/")
   public String home() {
-    return "home"; // Return the name of your front-end page (without the extension)
+    return "home";
   }
 
   @GetMapping("/about")
   public String about() {
-    return "about"; // Return the name of your front-end page (without the extension)
+    return "about";
   }
 
   @GetMapping("/application")
   public String application() {
-    return "application"; // Return the name of your front-end page (without the extension)
+    return "application";
   }
 
   @GetMapping("/dashboard")
   public String dashboard() {
-    return "dashboard"; // Return the name of your front-end page (without the extension)
+    return "dashboard"; 
   }
 
   @GetMapping("/login")
   public String login() {
-    return "login"; // Return the name of your front-end page (without the extension)
+    return "login"; 
   }
 
   @GetMapping("/registeremployee")
   public String register() {
-    return "register"; // Return the name of your front-end page (without the extension)
+    return "register"; 
   }
 
   @GetMapping("/createaccount")
   public String crreateaccount() {
-    return "createaccount"; // Return the name of your front-end page (without the extension)
+    return "createaccount"; 
   }
 
   @GetMapping("/approveclaimdetails")
   public String approveexpenseclaimdetails() {
-    return "approveexpenseclaimdetails"; // Return the name of your front-end page (without the extension)
+    return "approveexpenseclaimdetails"; 
+// Return the name of your front-end page saved in resources
   }
 }
